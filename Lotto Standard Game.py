86337a830e15597af1a47d8f0d3397c43f3b00dd
Pablo-IT <pawelgorski.ekonomia@gmail.com>
2023-03-12 22:49:36 +0100

    Replacing int input into usual input, after that checking if user input is numeric and converting it into int or printing an error message.

--- Lotto Standard Game.py
+++ Lotto Standard Game.py
@@ -36,15 +36,20 @@ while (nr < 6):
 #Draving six numbers choosen by user
 user_numbers = []
 nr = 0
+
 while nr < 6:
-    user_drawn = int(input('Please type a number from 1 to 49:'))
-    if (user_drawn < 1 or user_drawn > 49):
-        print('Number needs to be in range from 1 to 49.')
-    elif user_drawn not in user_numbers:
-        user_numbers.append(user_drawn)
-        nr += 1
+    user_drawn = input('Please type a number from 1 to 49:')
+    if user_drawn.isnumeric():
+        user_drawn = int(user_drawn)
+        if (user_drawn < 1 or user_drawn > 49):
+            print('Number needs to be in range from 1 to 49.')
+        elif user_drawn not in user_numbers:
+            user_numbers.append(user_drawn)
+            nr += 1
+        else:
+            print('This value has been already chosen.')
     else:
-        print('This value has been already chosen.')
+        print('Value has to be a number in range from 1 to 49.')
 
 #User results
 drawn_numbers.sort()
@@ -52,14 +57,17 @@ user_numbers.sort()
 
 result = []
 
-for nr in drawn_numbers:
-    if nr in user_numbers:
-        result.append(nr)
+for number in drawn_numbers:
+    if number in user_numbers:
+        result.append(number)
 
 result.sort()
 result_count = len(result)
 
 #Probability of user results
+probability_count_r = 1
+probability_draw_r = 1
+
 user_probability_range = numbers_count[0:result_count]
 user_numbers_range = range(result_count + 1)
 user_drawn_numbers = list(user_numbers_range)
@@ -67,17 +75,17 @@ user_drawn_numbers.pop(0)
 
 for number in user_probability_range:
     nr = number * 1
-    probability_count *= nr
+    probability_count_r *= nr
 
 for number in user_drawn_numbers:
     nr = number * 1
-    probability_to_draw *= nr
+    probability_draw_r *= nr
 
-userp = 1/(probability_count / probability_to_draw)
+userp = 1/(probability_count_r / probability_draw_r)
 userp = '{:.2%}'.format(userp)
 
 #Print results
-print('Probability of drawing a six is 1 to', probability)
+print('Probability of drawing a six is 1:', probability)
 
 if result_count > 0:
     print('Probability of drawing a: ', result_count, ' is: ', userp)
